3e3284c14a09a37dbe52e2c3dcc57a9f82c66edb
Iqbal Shah Nadiri <iqbalshahnadiri786@gmail.com>
2024-02-07 14:22:57 +0500

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-  const useGames = () => {
-    interface Game {
-        id: number;
-        name: string;
-      }
-      interface FetchGamesResponse {
-        count: number;
-        results: Game[];
-      }
+export interface Game {
+  id: number;
+  name: string;
+  background_image:string
+}
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+   
     
       const [games, setGames] = useState<Game[]>([]);
       const [error, setError] = useState("");
@@ -33,5 +36,5 @@ import { CanceledError } from "axios";
     
   }
   
-  export default useGames
+export default useGames
   
