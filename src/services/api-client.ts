c788da6a920493ac63c1ada647bd9adfa778e23a
Iqbal Shah Nadiri <iqbalshahnadiri786@gmail.com>
2024-02-14 13:33:04 +0500

    Update API key and add react-dotenv package

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:"https://api.rawg.io/api",
     params:{
-        key:'812aa52f0a6f4ee6a53269050412bf26'
+        key:`${import.meta.env.VITE_API_KEY}`
         }
 })
